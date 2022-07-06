7f9a4345a30432dafec50851ed3c0bd45d7465e9
rmrf <rmrf@rm.rf>
2022-07-07 03:10:57 +0800

    cors

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const axios = require("axios");
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   console.log("asdasd");
